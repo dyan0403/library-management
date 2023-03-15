8bce6f7b66314841f94ea1b1460f484f05ea8ba1
ngoctuds73 <123917622+ngoctuds73@users.noreply.github.com>
2023-03-15 08:40:25 +0700

    Add files via upload

---
+++ User_management.py
@@ -0,0 +1,50 @@
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def change_password(self, new_password):
+        self.password = new_password
+        print("Thay đổi mật khẩu thành công!")
+
+
+class Library:
+    def __init__(self):
+        self.users = []
+        self.logged_in_user = None
+
+    def register_user(self, username, password):
+        # Kiểm tra xem người dùng đã tồn tại hay chưa
+        for user in self.users:
+            if user.username == username:
+                print("Tài khoản đã tồn tại!")
+                return
+
+        # Đăng kí một người dùng mới
+        new_user = User(username, password)
+        self.users.append(new_user)
+        print("Đăng kí thành công!")
+
+    def login(self, username, password):
+        # Đăng nhập người dùng
+        for user in self.users:
+            if user.username == username and user.password == password:
+                self.logged_in_user = user
+                print("Đăng nhập thành công")
+                return
+        print("Sai mật khẩu hoặc tài khoản")
+
+    def logout(self):
+        # Đăng xuất người dùng
+        self.logged_in_user = None
+        print("Hẹn gặp lại!")
+
+    def change_password(self, current_password, new_password):
+        # Thay đổi mật khẩu người dùng đang đăng nhập
+        if self.logged_in_user:
+            if self.logged_in_user.password == current_password:
+                self.logged_in_user.change_password(new_password)
+            else:
+                print("Mật khẩu mới bị trùng với mật khẩu hiện tại")
+        else:
+            print("Bạn phải đăng nhập để đổi mật khẩu")
