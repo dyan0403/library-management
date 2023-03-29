3dbae7edbe22232829073e61e01deb4aa4866199
dyan0403 <127913060+dyan0403@users.noreply.github.com>
2023-03-29 08:56:56 +0700

    Update Account.py

--- Account.py
+++ Account.py
@@ -2,6 +2,9 @@
 ## id, password, status
 '''
 '''
+from datetime import datetime, date
+
+
 class Account:
     # def __init__(self, id, password, status=True). Them self cua cac add_info duoi dang None
     def __init__(self, email, password, status=None, type=None):
@@ -32,7 +35,7 @@ class Guest(Account):
 - Register: Borrow / Return Book
 - Read full Book
 '''
-class Student(Guest):
+class Student(Acccount):
   def __init__(self, email, ID, password, status=None, account_type="student"):
     super().__init__(email, password, status, account_type)
     self.ID = ID
@@ -87,7 +90,58 @@ class Student(Guest):
 - Download pdf file
 '''
 class Teacher(Account):
-    pass
+  def __init__(self, email, ID, password, status=None, account_type="student"):
+    super().__init__(email, password, status, account_type)
+    self.ID = ID
+    self.archives = {}
+
+  def view_info(self):
+    print(f"Name: {self.email}")
+    print("Password: " + "*" * len(self.password))
+    print(f"Status: {self.status}")
+    print(f"Type: {self.account_type}")
+    print(f"Archives: {self.archives}")
+
+
+  def borrow_book(self, book: Book):
+    if self.account_type not in ["student", "teacher"]:
+      return print('Bạn không đủ level mượn sách')    
+    elif self.status == "block":
+      print(f"Tài khoản {self.email} của bạn đã bị chặn quyền mượn sách.")
+    elif self.status == "cancel":
+      print("Tài khoản của bạn đã bị xóa.")
+    elif book.number_of_book <= 0:
+      print("Sách đã hết, vui lòng chọn sách khác.")
+    else:
+      print("Bạn có thể đọc quyển sách này bây giờ.")
+      choice = input("Bạn muốn mượn quyển sách này chứ ? (y/n) ")
+      if choice.lower() == "y" and book not in self.archives:
+          ngay_muon = date.today()
+          self.archives[book] = ngay_muon
+          book.number_of_book -= 1
+          print("Đã thêm sách thành công, bạn có thể đến thư viện mượn sách")
+
+  def return_book(self, book: Book):
+    if self.account_type not in ["student", "teacher"]:
+      print("Bạn không thể thực hiện chức năng này")
+    elif book not in self.archives:
+      print("Không tìm thấy thông tin sách muốn trả")
+    else:
+      ngay_tra = date.today()
+      thoi_diem_tra = datetime.combine(ngay_tra, datetime.time.min)
+      ngay_muon = self.archives[book]
+      tinh_trang = "đúng hạn" if thoi_diem_tra <= ngay_muon + timedelta(days=180) else "quá hạn"
+      print(f"Bạn đã trả sách {tinh_trang}")
+      del self.archives[book]
+      book.number_of_book += 1
+
+
+  def request_add_book(self):
+      request = input("Nhập tên sách yêu cầu được mua/cập nhập thêm: ")
+      print(f"{self.email} đã yêu cầu thêm sách {request}")
+
+  def dowload_PDF(self):
+      print("Bạn có thể tải file PDF")
 
 # Tai khoan cua admin
 '''
